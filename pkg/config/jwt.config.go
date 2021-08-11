433cbb3d5b219f64b86ed772185eac395de31f2a
Maxi Sanchez <maxsanchezg@gmail.com>
2021-08-11 13:54:52 -0300

    [#12] Create Jwt service

---
+++ pkg/config/jwt.config.go
@@ -0,0 +1,16 @@
+package config
+
+import (
+	"os"
+)
+
+type JwtConfig struct {
+	SECRET string 
+}
+
+func LoadJwtConfig() JwtConfig{
+
+	return JwtConfig{
+		SECRET: os.Getenv("JWT_SECRET"),
+	}
+}
---
+++ pkg/services/jwt.service.go
@@ -0,0 +1,23 @@
+package services
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/brianfiszman/GoFromZeroToHero/pkg/config"
+	"github.com/go-chi/jwtauth/v5"
+	"github.com/mitchellh/mapstructure"
+)
+
+var TokenAuth *jwtauth.JWTAuth = jwtauth.New("HS256", []byte(config.LoadJwtConfig().SECRET), nil)
+
+func CreateJwtToken(r *http.Request) string {
+	request := map[string]string{}
+	var jsonbody = map[string]interface{}{}
+
+	json.NewDecoder(r.Body).Decode(&request)
+	mapstructure.Decode(request, jsonbody)
+	_, tokenString, _ := TokenAuth.Encode(jsonbody)
+
+	return tokenString
+}
