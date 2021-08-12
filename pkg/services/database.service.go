c3b1cd1c9d788bc43215f5445a0d642c4131fae1
Brian Fiszman <fiszmanbrian@gmail.com>
2021-08-12 18:49:33 -0300

    [#13] Create DatabaseService

--- pkg/services/database.service.go
+++ pkg/services/database.service.go
@@ -5,27 +5,26 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/brianfiszman/GoFromZeroToHero/pkg/models"
+	"github.com/brianfiszman/GoFromZeroToHero/pkg/config"
 	"github.com/jackc/pgx/v4/pgxpool"
 )
 
-func ConnectDatabase () {
-	dbpool, err := pgxpool.Connect(context.Background(), os.Getenv("DATABASE_URL"))
-	
+type Database struct {
+	Config         config.DatabaseConfig
+	ConnectionPool *pgxpool.Pool
+}
+
+func (d *Database) ConnectDatabase() {
+	dbpool, err := pgxpool.Connect(context.Background(), d.Config.GetConnectionString())
+
+	d.ConnectionPool = dbpool
+
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
-		
 	}
 
-	defer dbpool.Close()
-	
-	ticket := models.Ticket{
-		Id: "pija",
-	}
-
-	fmt.Print(ticket);
+	defer d.ConnectionPool.Close()
 
 	fmt.Println("Connected to Database")
-
 }
