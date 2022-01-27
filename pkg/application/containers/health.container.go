7a88985e272c04d6be7bc80446fbad738cedee29
Brian <fiszmanbrian@gmail.com>
2022-01-27 19:06:41 -0300

    [#36] Create HealthContainer

--- pkg/application/containers/health.container.go
+++ pkg/application/containers/health.container.go
@@ -1,11 +1,37 @@
 package containers
 
-import "github.com/brianfiszman/GoFromZeroToHero/pkg/domain/interfaces"
+import (
+	"github.com/brianfiszman/GoFromZeroToHero/pkg/application/controllers"
+	"github.com/brianfiszman/GoFromZeroToHero/pkg/domain/interfaces"
+	"github.com/brianfiszman/GoFromZeroToHero/pkg/domain/services"
+	"github.com/brianfiszman/GoFromZeroToHero/pkg/presentation/routers"
+)
 
 type HealthContainer struct {
-	Database interfaces.Database
+	Database   interfaces.Database
+	Router     routers.HealthRouter
+	Controller controllers.HealthController
+	Service    services.HealthService
 }
 
-func NewHealthContainer() (h HealthContainer) {
+func NewHealthContainer(d interfaces.Database) (h HealthContainer) {
+	var healthService services.HealthService = services.HealthService{
+		Database: d,
+	}
+
+	var healthController controllers.HealthController = controllers.HealthController{
+		HealthService: healthService,
+	}
+
+	var healthRouter routers.HealthRouter = routers.HealthRouter{
+		HealthController: healthController,
+	}
+
+	healthRouter.Handler = healthRouter.NewHealthRouter()
+
+	h.Router = healthRouter
+	h.Controller = healthController
+	h.Service = healthService
+
 	return
 }
